0f187ac3d694be008b4b26c7673ff367b5bb65f9
Ken Minami <kenminami.dev@gmail.com>
2022-02-23 23:35:04 +0900

    add home screen.

---
+++ lib/screen/components/bar_chart.dart
@@ -0,0 +1,155 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class _BarChart extends StatelessWidget {
+  const _BarChart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 10,
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+    enabled: false,
+    touchTooltipData: BarTouchTooltipData(
+      tooltipBgColor: Colors.transparent,
+      tooltipPadding: const EdgeInsets.all(0),
+      tooltipMargin: 8,
+      getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+          ) {
+        return BarTooltipItem(
+          rod.y.round().toString(),
+          const TextStyle(
+            color: Color(0xff7589a2),
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+    ),
+  );
+
+  FlTitlesData get titlesData => FlTitlesData(
+    show: true,
+    bottomTitles: SideTitles(
+      showTitles: true,
+      getTextStyles: (context, value) => const TextStyle(
+        color: Color(0xff7589a2),
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+      margin: 20,
+      getTitles: (double value) {
+        switch (value.toInt()) {
+          case 0:
+            return 'Mn';
+          case 1:
+            return 'Te';
+          case 2:
+            return 'Wd';
+          case 3:
+            return 'Tu';
+          case 4:
+            return 'Fr';
+          case 5:
+            return 'St';
+          case 6:
+            return 'Sn';
+          default:
+            return '';
+        }
+      },
+    ),
+    leftTitles: SideTitles(showTitles: false),
+    topTitles: SideTitles(showTitles: false),
+    rightTitles: SideTitles(showTitles: false),
+  );
+
+  FlBorderData get borderData => FlBorderData(
+    show: false,
+  );
+
+  List<BarChartGroupData> get barGroups => [
+    BarChartGroupData(
+      x: 0,
+      barRods: [
+        BarChartRodData(
+            y: 4, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 1,
+      barRods: [
+        BarChartRodData(
+            y: 5, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 2,
+      barRods: [
+        BarChartRodData(
+            y: 3, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(
+            y: 3, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 4,
+      barRods: [
+        BarChartRodData(
+            y: 3, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 5,
+      barRods: [
+        BarChartRodData(
+            y: 6, colors: [Colors.teal, Colors.teal])
+      ],
+      showingTooltipIndicators: [0],
+    ),
+  ];
+}
+
+class BarChartSample3 extends StatefulWidget {
+  const BarChartSample3({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => BarChartSample3State();
+}
+
+class BarChartSample3State extends State<BarChartSample3> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 5/2,
+      child: Card(
+        elevation: 0,
+        color: Colors.transparent,
+        child: const _BarChart(),
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_online_learning_app/screen/components/bar_chart.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 /// Home screen.
 class HomeScreen extends StatefulWidget {
@@ -7,8 +10,150 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/images/man.png", width: 35.0),
+                        const SizedBox(width: 10.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Aaron Jones", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15.0)),
+                            Text("Data Scientist", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 10.0, color: Colors.grey)),
+                          ],
+                        )
+                      ],
+                    ),
+                    Icon(Icons.notifications_active_outlined),
+                  ],
+                ),
+                const SizedBox(height: 30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Your Course", style: TextStyle(fontWeight: FontWeight.w700)),
+                    Text("See all", style: TextStyle(fontWeight: FontWeight.w500, color: Colors.blueAccent)),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                _makeYourCourses(size.width - 40.0),
+                const SizedBox(height: 30.0),
+                Text("Statistics", style: TextStyle(fontWeight: FontWeight.w700)),
+                const SizedBox(height: 20.0),
+                BarChartSample3(),
+                const SizedBox(height: 30.0),
+                Text("For You", style: TextStyle(fontWeight: FontWeight.w700)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        margin: EdgeInsets.all(20.0),
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+            selectedColor: Colors.purple,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.design_services),
+            title: Text("Courses"),
+            selectedColor: Colors.pink,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+            selectedColor: Colors.orange,
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Profile"),
+            selectedColor: Colors.teal,
+          ),
+        ],
+      ),
+    );
   }
+}
+
+/// Make your courses widget.
+Widget _makeYourCourses(double width) {
+  double cardWidth = (width/2) - 5.0;
+  return Column(
+    children: [
+      Row(
+        children: [
+          _makeCourseCard("Data Science with Python", Colors.teal.withOpacity(0.5), cardWidth, 0.7),
+          const SizedBox(width: 10.0),
+          _makeCourseCard("Brain Science", Colors.red.withOpacity(0.5), cardWidth, 0.4),
+        ],
+      ),
+    ],
+  );
+}
+
+/// Make course card widget.
+Widget _makeCourseCard(String title, Color color, double width, double progress) {
+  return Container(
+    width: width,
+    height: 170.0,
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 17.0)),
+                const SizedBox(height: 5.0),
+                Text("9 hours progress", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 10.0)),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Row(
+              children: [
+                CircularPercentIndicator(
+                  radius: 20.0,
+                  lineWidth: 8.0,
+                  animation: true,
+                  percent: progress,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  progressColor: Colors.white,
+                  backgroundColor: Colors.black45,
+                ),
+                const SizedBox(width: 5.0),
+                Text((progress*100).toString()+"%", style: TextStyle(fontWeight: FontWeight.w700, color: Colors.white))
+              ],
+            ),
+          ),
+        ],
+      )
+    ),
+  );
 }
